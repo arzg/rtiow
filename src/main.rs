66d8fb3fd52d3f5b90f5e22a15b73c917190c139
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-04-05 19:38:44 +1000

    Add basic graphics ‘Hello World’

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+const WIDTH: u32 = 200;
+const HEIGHT: u32 = 100;
+
+fn main() -> anyhow::Result<()> {
+    let mut img: image::RgbImage =
+        image::ImageBuffer::from_pixel(WIDTH, HEIGHT, image::Rgb([0, 0, 0]));
+
+    for (y, row) in img.rows_mut().rev().enumerate() {
+        for (x, pixel) in row.enumerate() {
+            let r = x as f64 / f64::from(WIDTH);
+            let g = y as f64 / f64::from(HEIGHT);
+            let b: f64 = 0.2;
+
+            *pixel = image::Rgb([
+                (255.999 * r).round() as u8,
+                (255.999 * g).round() as u8,
+                (255.999 * b).round() as u8,
+            ]);
+        }
+    }
+
+    img.save("output.png")?;
+
+    Ok(())
+}
