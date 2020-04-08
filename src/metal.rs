7c3c5251e5696ef29052b003305436a6e782a7d9
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-04-08 20:50:20 +1000

    Add support for fuzzy reflections on metal

--- src/metal.rs
+++ src/metal.rs
@@ -1,11 +1,12 @@
 #[derive(Debug)]
 pub struct Metal {
     color: crate::Color,
+    fuzz: crate::Float,
 }
 
 impl Metal {
-    pub fn new(color: crate::Color) -> Self {
-        Self { color }
+    pub fn new(color: crate::Color, fuzz: crate::Float) -> Self {
+        Self { color, fuzz }
     }
 }
 
@@ -16,11 +17,10 @@ impl crate::Material for Metal {
         hit_record: crate::HitRecord,
     ) -> Option<(crate::Ray, &crate::Color)> {
         let reflected = crate::reflect(&ray.direction().normalize(), hit_record.normal());
-        let scattered = crate::Ray::new(*hit_record.position(), reflected);
-        let attenuation = &self.color;
+        let scattered = crate::Ray::new(*hit_record.position(), reflected + self.fuzz * crate::rand_unit_vector());
 
         if scattered.direction().dot(hit_record.normal()) > 0.0 {
-            Some((scattered, attenuation))
+            Some((scattered, &self.color))
         } else {
             None
         }
